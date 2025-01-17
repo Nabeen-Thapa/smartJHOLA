ed0bb8c4721fbaf149c5df55ac46af7a74d8e6e8
Nabeen-Thapa <nabeen2058@gmail.com>
2025-01-17 15:03:44 +0545

    add changes on registration and forntend

--- src/users/utils/user-register-validate.ts
+++ src/users/utils/user-register-validate.ts
@@ -4,7 +4,6 @@ export const userValidationSchema =joi.object({
     name:joi.string().trim().required(),
     username: joi.string().alphanum().trim().min(4).max(100).required(),
     email:joi.string().email().trim().required(),
-    //phone:joi.string().trim().required(),
     phone: joi.alternatives().try(joi.string(), joi.number()).custom((value) => String(value)).required(),
     age: joi.number().integer().allow(null, " ").optional(),
     gender : joi.string().allow(null, " ").optional(),
