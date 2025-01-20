a03a310769dec49aad6fa371a47896ebc4cfb478
Nabeen-Thapa <nabeen2058@gmail.com>
2025-01-20 09:57:08 +0545

    add view products feature

--- src/products/controllers/view-category.ts
+++ src/products/controllers/view-category.ts
@@ -7,23 +7,25 @@ import { smartCategory } from "../entities/productsCategory";
 const viewCategory:Router = express.Router();
 
 viewCategory.get('/viewCategory', async(req:Request, res:Response):Promise<void>=>{
-    const {username, password} =req.body;
-    if(!username || !password){
-        res.status(StatusCodes.BAD_REQUEST).json({message : "enter username and password"});
-        return;
-    } 
 
-    if(username !== "admin" && password !=="admin12"){
-        res.status(StatusCodes.BAD_REQUEST).json({message :"username and password are not match"});
-        return;
-    }
+
+    // const {username, password} =req.body;
+    // if(!username || !password){
+    //     res.status(StatusCodes.BAD_REQUEST).json({message : "enter username and password"});
+    //     return;
+    // } 
+
+    // if(username !== "admin" && password !=="admin12"){
+    //     res.status(StatusCodes.BAD_REQUEST).json({message :"username and password are not match"});
+    //     return;
+    // }
     try {
         
     //accessing users form database
     const categoryRepositery = smartConnection.getRepository(smartCategory);
     const categories =await categoryRepositery.find();
     if (categories.length === 0) {
-        res.status(StatusCodes.NO_CONTENT).json({message: "No users found"});
+        res.status(StatusCodes.NO_CONTENT).json({message: "No catagories found"});
         return;
     }
     res.status(200).json({
--- src/products/routers/product-routes.ts
+++ src/products/routers/product-routes.ts
@@ -2,12 +2,14 @@ import express, { Router } from "express";
 import addCategory from "../controllers/add-category";
 import addProduct from "../controllers/add-products";
 import viewCategory from "../controllers/view-category";
+import viewProduct from "../controllers/view-product";
 
 const productRoutes:Router = express.Router();
 
 productRoutes.use("/smartjhola",addCategory);
 productRoutes.use("/smartjhola",addProduct);
 productRoutes.use("/smartjhola",viewCategory);
+productRoutes.use("/smartjhola",viewProduct);
 
 
 export default productRoutes;
