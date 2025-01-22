c5c8de458e9a472d208459cb6cae610928c3b9f5
Nabeen-Thapa <nabeen2058@gmail.com>
2025-01-22 23:41:38 +0545

    add update-user code

--- src/users/controllers/delete-user.ts
+++ src/users/controllers/delete-user.ts
@@ -3,7 +3,7 @@ import express, { Request, Response, Router } from "express";
 const deleteUser :Router = express.Router();
 
 deleteUser.delete("/delete-user", async(req:Request, res:Response):Promise<void>=>{
-
+    
 })
 
 export default deleteUser;
