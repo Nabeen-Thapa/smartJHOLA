de982f7fb13d0c0f40b880b02168a826ec1ce9ff
Nabeen-Thapa <nabeen2058@gmail.com>
2025-01-25 15:55:52 +0545

    add user registration and login check middleware

---
+++ src/common/middleware/check-registration.ts
@@ -0,0 +1,26 @@
+import express, { Response } from "express";
+import { smartConnection } from "../db/db-connection-config";
+import { smartAdmin } from "../../admin/entities/adminDetails";
+import { smartUser } from "../../users/entities/userDetails";
+import { StatusCodes } from "http-status-codes";
+import logger from "../utils/logger";
+
+export const isRegister = async (username: string, res: Response): Promise<void> => {
+    try {
+        const getAdminRepo = smartConnection.getRepository(smartAdmin);
+        const getUserRepo = smartConnection.getRepository(smartUser);
+
+        const isAdminExist = await getAdminRepo.findOne({ where: { username } });
+        if (!isAdminExist) {
+            const isUserExist = await getUserRepo.findOne({ where: { username } });
+            if (!isUserExist) {
+                res.status(StatusCodes.NOT_FOUND).json({message :"you are not registered"});;
+                return;
+            }
+        }
+        res.status(StatusCodes.OK);
+    } catch (error) {
+        logger.error("error duirng register chacking: ", error);
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+}
