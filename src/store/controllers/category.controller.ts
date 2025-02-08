4f3d768cb8161e4ffc28f534c7893a1821e74d54
Nabeen-Thapa <nabeen2058@gmail.com>
2025-02-08 22:26:33 +0545

    add update category controller

--- src/store/controllers/category.controller.ts
+++ src/store/controllers/category.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { addCategory, viewCategory } from "../services/category.services";
+import { addCategory, viewCategory,updateCategory } from "../services/category.services";
 import { StatusCodes } from "http-status-codes";
 import logger from "../../common/utils/logger";
 
@@ -20,6 +20,20 @@ export const readCategory = async(req:Request, res:Response)=>{
     const {categoryId} =req.body;
    try {
     const category = await viewCategory(categoryId);
+    res.status(StatusCodes.OK).json({
+        success :true,
+        category : category
+    })
+   } catch (error) {
+    logger.error("Error during view category:", error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: error.message });
+   }
+}
+
+export const updatedCategord = async(req:Request, res:Response)=>{
+    const {categoryId, categoryName, categoryDescription} =req.body;
+   try {
+    const category = await updateCategory(categoryId,categoryName, categoryDescription);
     res.status(StatusCodes.OK).json({
         success :true,
         category : category
