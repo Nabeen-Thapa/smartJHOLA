6c7c1dcb511bf568d6f1acc534beb3933d91ede5
Nabeen-Thapa <nabeen2058@gmail.com>
2025-02-18 21:54:46 +0545

    add coupon code check service

--- src/store/services/coupon.services.ts
+++ src/store/services/coupon.services.ts
@@ -2,6 +2,9 @@ import { Response } from "express"
 import { smartConnection } from "../../common/db/db-connection-config";
 import { smartCoupon } from "../entities/discount.coupon";
 import StatusCodes from "http-status-codes";
+import { smartUser } from "../../users/entities/userDetails";
+import { smartCart } from "../entities/AddToCart";
+import { smartToken } from "../../users/entities/smartUserToken";
 
 export const addCouponCode =async (couponCode:number,discountPrecentage:number, ExpireDate:Date, res:Response)=>{
     if(!couponCode || !ExpireDate){
@@ -29,10 +32,37 @@ export const addCouponCode =async (couponCode:number,discountPrecentage:number, 
     }
 }
 
-export const checkCouponCode =async(couponCode, totalAmount) =>{
+export const checkCouponCode =async(couponCode:number, totalAmount:number,user: smartUser, res:Response) =>{
     try {
-        
+        const getCouponRepo = smartConnection.getRepository(smartCoupon);
+        const getCartRepo =  smartConnection.getRepository(smartCart);
+        const getTokenRepo = smartConnection.getRepository(smartToken);
+
+        const isCouponExist = await getCouponRepo.findOne({where :{couponCode}});
+        if(couponCode !== isCouponExist?.couponCode){
+            throw new Error("coupon is not mached");
+        }
+
+            const isUserLoggedIn = await getTokenRepo.findOne({ where: { userId:user.userId } });
+            if (!isUserLoggedIn) {
+                throw new Error("you are not logged in");
+            }
+            //get cart items
+            const cartItems = await getCartRepo.find({where:{user :{userId : user.userId}}});
+            if (cartItems.length === 0) {
+               throw new Error("you don't add any items in cart");
+            }
+            const totalPrice = cartItems.reduce((sum, item) => sum + item.total_price, 0);
+            const totalPriceAfterDiscount =totalPrice - (totalPrice *0.1);
+            return {
+                success: true,
+                data: cartItems,
+                totalPrice :totalPrice,
+                totalPriceAfterDiscount : totalPriceAfterDiscount
+            };
+
     } catch (error) {
-        
+        console.log("error duirng coupon check :", error);
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({message: "internal server errro during add check: ", error});
     }
 }
