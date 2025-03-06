ce609865d302ca975fc3aa549d0a75a42b8f8d86
Nabeen-Thapa <nabeen2058@gmail.com>
2025-03-06 15:07:09 +0545

    Add enum for error message

---
+++ src/types/errorMessage.enum.ts
@@ -0,0 +1,13 @@
+export enum errorMessage {
+    OK = "ok",
+    CREATED ="successfully created",
+    ACCEPTED ="accepted",
+    NOT_ACCEPTABLE ="not accepted",
+    NO_CONTENT ="there not any content",
+    INTERNAL_SERVER_ERROR = "internal server error",
+    SERVICE_UNAVAILABLE = "request service is not avilable",
+    CONFLICT = "failed duw to data conflict",
+    UNAUTHORIZED = "you are not authorized",
+    BAD_REQUEST = "bad request"
+
+}
